3bfb32a4ba026a15bef83d8b17bfc73ec1899615
Atnanasi <ys2000pro@gmail.com>
2016-10-10 00:36:01 +0900

    Read config file

--- source/app.d
+++ source/app.d
@@ -1,7 +1,13 @@
 import vibe.d;
+import std.json;
+import std.string;
+import std.file;
 import sibwive.index;
 
 shared static this() {
+	JSONValue config_system = parseJSON(chomp(readText("config/system.json")));
+	JSONValue config_user = parseJSON(chomp(readText("config/user.json")));
+
 	auto router = new URLRouter;
 	router.get("/", &index);
 
